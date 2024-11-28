b8023d6f8138c4a67581bd59d392bab9b81a87f7
MaquinsFer <166024803+MaquinsFer@users.noreply.github.com>
2024-11-28 14:02:57 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,35 @@
-let nomeUsuario = "";
-let elemento = document.querySelector("#nome-usuario");
-
-while(nomeUsuario == ""){
-    nomeUsuario = prompt("Qaul o seu nome");
-}
-
-if(nomeUsuario == null){
-    elemento.textContent = 'Seja muito Bem-vindo.';
-}else{
-    elemento.textContent = nomeUsuario;
-}
+const products = [
+    {
+      id: 1,
+      name: "Camiseta Masculina",
+      price: 59.99,
+      image: "image.jpg",
+    },
+    {
+      id: 2,
+      name: "Vestido Feminino",
+      price: 99.99,
+      image: "assets/img/vestido.jpg",
+    },
+  ];
+  
+  const productContainer = document.querySelector(".product-list");
+  
+  products.forEach((product) => {
+    const productCard = document.createElement("div");
+    productCard.classList.add("product-card");
+  
+    productCard.innerHTML = `
+      <img src="${product.image}" alt="${product.name}">
+      <h3>${product.name}</h3>
+      <p>R$ ${product.price.toFixed(2)}</p>
+      <button onclick="addToCart(${product.id})">Comprar</button>
+    `;
+  
+    productContainer.appendChild(productCard);
+  });
+  
+  function addToCart(productId) {
+    const product = products.find((p) => p.id === productId);
+    alert(`Você adicionou "${product.name}" ao carrinho!`);
+  }
